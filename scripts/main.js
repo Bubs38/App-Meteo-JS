39990618b975ed255cb042f76c0e6d93918b79ec
Bubs38 <burgain.aurelien@gmail.com>
2022-04-19 23:25:17 +0200

    Version finale

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
-const ClefApi = "";
+import tabJoursEnOrdre from "./Utilitaire/gestionTemps.js";
+
+const ClefApi = ""; // Rentrer la clef Api
 let resultatsApi;
 
 const temps = document.querySelector(".temps");
 const temperature = document.querySelector(".temperature");
 const localisation = document.querySelector(".localisation");
 const heure = document.querySelectorAll(".heure-nom-prevision");
-const tempPourH = document.querySelectorAll("heure-prevision-valeur");
+const tempPourH = document.querySelectorAll(".heure-prevision-valeur");
+const joursDiv = document.querySelectorAll(".jour-prevision-nom");
+const tempJourDiv = document.querySelectorAll(".jour-prevision-temp");
+const imgIcone = document.querySelector(".logo-meteo");
+const chargementContainer = document.querySelector(".overlay-icone-chargement");
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -54,6 +60,31 @@ const appelApi = (long, lat) => {
       }
 
       // temp pour 3h
-      for(let j = 0; j < tempPourH.length)
+      for (let j = 0; j < tempPourH.length; j++) {
+        tempPourH[j].innerText = `${Math.trunc(
+          resultatsApi.hourly[j * 3].temp
+        )}°`;
+      }
+
+      // trois premières lettres du jour
+      for (let k = 0; k < tabJoursEnOrdre.length; k++) {
+        joursDiv[k].innerText = tabJoursEnOrdre[k].slice(0, 3);
+      }
+
+      // temp par jour
+      for (let m = 0; m < 7; m++) {
+        tempJourDiv[m].innerText = `${Math.trunc(
+          resultatsApi.daily[m + 1].temp.day
+        )}°`;
+      }
+
+      // Icone dynamique
+      if (heureActuelle >= 6 && heureActuelle < 21) {
+        imgIcone.src = `ressources/jour/${resultatsApi.current.weather[0].icon}.svg`;
+      } else {
+        imgIcone.src = `ressources/nuit/${resultatsApi.current.weather[0].icon}.svg`;
+      }
+
+      chargementContainer.classList.add("disparition");
     });
 };
